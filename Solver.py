4761572d0bb0aa807bb0f5c17471933ed5a624e1
sofiadagli <sofiadagli9@gmail.com>
2026-06-17 16:43:53 +0300

    Add results and update solver

--- Solver.py
+++ Solver.py
@@ -24,10 +24,18 @@ def element_stiffness_triangle(node_coords, k=1.0):
     x1, y1 = node_coords[0, 0], node_coords[0, 1]
     x2, y2 = node_coords[1, 0], node_coords[1, 1]
     x3, y3 = node_coords[2, 0], node_coords[2, 1]
-    area = []# element area
+    area = 0.5*abs(x1*(y2-y3)-x2*(y3-y1)-x3*(y1-y2))# element area
 
+    y23 = y2 - y3
+    y31 = y3 - y1
+    y12 = y1 - y2
+    
+    x32 = x3 - x2
+    x13 = x1 - x3
+    x21 = x2 - x1
+    
     # Shape function derivatives (constant over element)
-    B = [] #matrix B
+    B = 1/(2*area)*np.array ([[y23, y31, y12], [x32, x13, x21]])
     Ke = k * area * (B.T @ B)
     return Ke
 
@@ -50,12 +58,16 @@ def assemble_global(nodes, elems, k=1.0):
         conn = elems[e]
         coords = nodes[conn, :2]  # take x,y only
         Ke = element_stiffness_triangle(coords, k=k)
+        
         for i_local, i_global in enumerate(conn):
             for j_local, j_global in enumerate(conn):
-                # Assemble global Matrix K
-                K=[]
+                # Assemble global Matrix
+                rows.append(i_global)
+                cols.append(j_global)
+                data.append(Ke[i_local,j_local])
+                
+    K = sp.coo_matrix((data,(rows,cols)), shape=(nnodes, nnodes)).tocsr()
 
-    K = [] 
     return K
 
 
@@ -65,20 +77,47 @@ def apply_dirichlet(K, f, bc_nodes, bc_values):
     bc_nodes: array of node indices
     bc_values: array of prescribed values
     """
-    K = K.tocsr().copy()
+    K = K.tolil(copy=True)
     f = f.copy()
-    for node, val in zip(np.atleast_1d(bc_nodes), np.atleast_1d(bc_values)):
-        #modify K and f accordingly    
-        K[node, node] = []
-        f[node] = []
-    return K, f
+    
+    for node, val in zip(bc_nodes, bc_values):
+        
+        #modify K and f accordingly   
+        col = K[:, node].toarray().ravel()
+        f -= col * val
+        
+        K[node, :] = 0
+        K[:, node] = 0
+        K[node, node] = 1
+        f[node] = val
+        
+    return K.tocsr(), f
+
+def apply_dirichlet_penalty(K, f, bc_nodes, bc_values, alpha_factor=1e4):
+
+    K_mod = K.tolil(copy=True)
+    f_mod = f.copy()
+
+    Kmax = abs(K_mod).max()
+    alpha = Kmax * alpha_factor
+
+    for node, value in zip(bc_nodes, bc_values):
+        K_mod[node, node] += alpha
+        f_mod[node] += alpha * value
+
+    return K_mod.tocsr(), f_mod
+
 
 def apply_heat_flux(f, nodes, elems, heat_flux_bcs):
     """
     Apply Neumann (heat flux) BCs to load vector.
     Each BC: (elem_id, edge_id, q)
     """
+    f = f.copy()
     for elem_id, edge_id, q in heat_flux_bcs:
+        
+        print(f"HeatFlux -> element {elem_id+1}, edge {edge_id}, q={q}")
+        
         conn = elems[elem_id]
         coords = nodes[conn, :2]
         edge_nodes = {
@@ -90,7 +129,7 @@ def apply_heat_flux(f, nodes, elems, heat_flux_bcs):
         x1, y1 = coords[n1]
         x2, y2 = coords[n2]
         L = np.hypot(x2-x1, y2-y1)
-        fe = [] # linear edge shape functions
+        fe = q * L * np.array([0.5, 0.5]) # linear edge shape functions
         f[conn[edge_nodes]] += fe
     return f
 
@@ -100,7 +139,12 @@ def apply_convection(K, f, nodes, elems, conv_bcs):
     Apply Robin (convection) BCs to load vector & matrix K.
     Each BC: (elem_id, edge_id, h, Tinf)
     """
+    K = K.tolil()
+    f = f.copy()
     for elem_id, edge_id, h, Tinf in conv_bcs:
+        
+        print(f"Convection -> element {elem_id+1}, edge {edge_id}, h={h}, Tinf={Tinf}")
+        
         conn = elems[elem_id]
         coords = nodes[conn, :2]
         edge_nodes = {
@@ -111,11 +155,21 @@ def apply_convection(K, f, nodes, elems, conv_bcs):
         n1, n2 = edge_nodes
         x1, y1 = coords[n1]
         x2, y2 = coords[n2]
+       
+        L = np.hypot(x2-x1, y2-y1)
         #Modify K and F accordingly
-        K = []
-        f = []
-    return K, f
+      
+        Ke = h * L / 6.0 * np.array([[2, 1],
+                                     [1, 2]])
+        fe = h * L * Tinf / 2.0 * np.array([1.0, 1.0])
 
+        edge_conn = conn[edge_nodes]
+        for i_local, i_global in enumerate(edge_conn):
+            f[i_global] += fe[i_local]
+            for j_local, j_global in enumerate(edge_conn):
+                K[i_global, j_global] += Ke[i_local, j_local]
+
+    return K.tocsr(), f
 
 def solve_system(K, f):
     """Solve the linear system Ku=f"""
--- main.py
+++ main.py
@@ -9,26 +9,35 @@ Main Script
 import numpy as np
 from PreProcessor import read_input_file
 from Solver import assemble_global, apply_convection, apply_dirichlet
+from Solver import apply_dirichlet_penalty
 from Solver import apply_heat_flux, solve_system
 from PostProcessor import plot_mesh, plot_mesh_interactive, plot_temperature_field
 from PostProcessor import export_temperature_csv
 
 
 # Import model info
-nodes, elems, materials, k, bcs = read_input_file('Ex1.semfe')
+nodes, elems, materials, k, bcs = read_input_file('validation.semfe')
 
 # Check Mesh Quality
 plot_mesh_interactive(nodes, elems, show=True, filename='interactive_mesh2.html')
 
 # Assemble global
 K = assemble_global(nodes, elems, k=k)
+f0 = np.zeros(nodes.shape[0])
 
 # Apply BCs
-bc_nodes = [node for node, val in bcs['temperature']]
-bc_values = [val for node, val in bcs['temperature']]
-Kmod, fmod = apply_dirichlet(K, np.zeros(nodes.shape[0]), bc_nodes, bc_values)
-fmod       = apply_heat_flux(fmod, nodes, elems, bc_values)
-Kmod, fmod = apply_convection(K, fmod, nodes, elems, bc_values)
+# APPLY CONVECTION (Robin)
+conv_bcs = bcs.get("convection", [])
+Kc, fc = apply_convection(K, f0, nodes, elems, conv_bcs)
+# APPLY HEAT FLUX (Neumann)
+flux_bcs = bcs.get("heat_flux", [])
+f2 = apply_heat_flux(fc, nodes, elems, flux_bcs)
+# APPLY DIRICHLET (Strong)
+temp_bcs = bcs.get("temperature", [])
+bc_nodes = [node for node, val in temp_bcs]
+bc_values = [val  for node, val in temp_bcs]
+
+Kmod, fmod = apply_dirichlet(Kc, f2, bc_nodes, bc_values)
 
 #
 u = solve_system(Kmod, fmod)
